51327feb2f922e874432c71a28b2f54a32ddc08e
Vraj Patel <155692667+Vrajp071@users.noreply.github.com>
2024-03-19 18:38:52 -0600

    Add files via upload

---
+++ Patelv66_Assignment 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is even number");
+                evenSum += i;
+            }
+            else
+            {
+                Console.WriteLine($"{i} is odd number");
+                oddSum += i;
+            }
+        }
+
+        Console.WriteLine($"The sum of even numbers is {evenSum}");
+        Console.WriteLine($"The sum of odd numbers is {oddSum}");
+    }
+}
